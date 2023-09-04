e8bf6633b75e7d503de5d3a439316a1200cfc506
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-09-04 12:40:54 +0200

    add difficulty

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,7 @@ Il computer deve generare 16 numeri casuali nello stesso range della difficoltà
 In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
 BONUS:
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
 difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
@@ -26,22 +27,37 @@ difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 cas
 const fieldEl = document.querySelector('.container_field');
 
 let limit = 100
+const bombe = 16
+const listaBombe = []
+let points = 0;
 
 
- function griglia(limit) {
+
+for (let i = 0; i < bombe; i++) {
+    const randomN = Math.floor(Math.random() * limit);
+    listaBombe.push(randomN)
+    
+}
+
+console.log(listaBombe);
+
+
+function griglia(limit) {
     
     for (let i = 0; i < limit; i++) {
         const casellaEL = document.createElement('div')
         casellaEL.classList.add('box')
         casellaEL.append(i + 1)
         fieldEl.append(casellaEL)
+        casellaEL.style.width = `calc(100% / ${Math.sqrt(limit)})`
 
-        casellaEL.addEventListener('click', function(){
-            casellaEL.classList.toggle('bg_active') 
-            casellaEL.append()
-            console.log(`e stata attivata la casella ${i + 1}`);
-          
-        })
+        
+            casellaEL.addEventListener('click', function(){
+                casellaEL.classList.toggle('bg_active') 
+                casellaEL.append()
+                console.log(`e stata attivata la casella ${i + 1}`);
+            
+            })
     } 
     
 }
@@ -50,9 +66,11 @@ let limit = 100
 const bottone = document.querySelector('.btn')
 
 bottone.addEventListener('click', function(){
+    const difficulty = document.getElementById('difficulty').value
+    limit = difficulty
     griglia(limit)
-
 }, {once : true})
+
 
 
 
